18f2fe17e4bc1beea27bd6622082eb18c210e602
AdelShawqai <adel.shwqai@gmail.com>
2025-08-26 02:06:13 +0200

    Updating TestClass1 Break!!

--- src/test/java/testPackage/TestClass.java
+++ src/test/java/testPackage/TestClass.java
@@ -4,15 +4,25 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import java.sql.Driver;
+
 public class TestClass {
     @Test
     public void test (){
         WebDriver driver = new ChromeDriver();
+        driver.navigate().to("https://www.google.com");
         System.out.println(driver);
         System.out.println("Test method executed");
-        driver.navigate();
-        String s = ("https://www.google.com/");
-        driver.get(s);
+        String Gtitle = driver.getTitle();
+        System.out.println(Gtitle);
+        driver.get("https://www.facebook.com");
+        String Ftitle = driver.getTitle();
+        System.out.println(Ftitle);
+        driver.navigate().back();
+        String GGtitle = driver.getTitle();
+        System.out.println(GGtitle);
+
+
     driver.quit();
     }
 }
